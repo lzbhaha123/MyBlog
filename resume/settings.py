a459521813e68d3eb129b28418b9455973936847
lzbhaha123 <845381951@qq.com>
2022-07-17 15:51:11 +1200

    v1.1

--- resume/settings.py
+++ resume/settings.py
@@ -127,3 +127,6 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_UPLOAD_PATH = "media/"
+
